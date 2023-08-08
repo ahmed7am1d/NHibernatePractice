8e416ed0b8ccc83540a5a8446ede86ae8c90031e
Ahmed Taha <ahmadhamidty@gmail.com>
2023-08-09 02:41:24 +0300

    RetriveById - Insert data to DB

---
+++ NHibernetCreateSession/NHibernateInsertData/Models/Wallet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernetCreateSession.Models;
+public class Wallet
+{
+    public virtual int Id { get; set; }
+    //I am sure that holder is required and exists
+    public virtual string Holder { get; set; } = null!;
+    public virtual decimal Balance { get; set; }
+
+    public override string ToString()
+    {
+        return $"[{Id}] {Holder} ({Balance:C})";
+    }
+}
